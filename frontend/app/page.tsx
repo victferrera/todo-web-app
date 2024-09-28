581f0686cdf952211d24e06506586cec437d5e32
Vitor Ferreira <victor-bash2@hotmail.com>
2024-09-28 17:08:55 -0300

    fixing listing and sorting array

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -20,19 +20,25 @@ export default function Home() {
     fetch();
   }, []);
 
+  useEffect(() => {
+    setTodosAux(todos.sort((a, b) => a.id - b.id));
+  }, [todos])
+
   const fetch = async () => {
     setLoading(true)
-    const response = (await getAllTodos());
+    const response = (await getAllTodos()).sort((a, b) => a.id - b.id);
     setTodos(response);
     setTodosAux(response);
+    setOnlyOpenTasks(2);
     setLoading(false)
   };
 
-  const callback = async () => {
-    await fetch();
-  };
+  const handleUpdateTodos = async() => {
+    const response = await getAllTodos();
+    setTodos(response);
+  }
 
-  const handleOnlyOpenTasksClick = () => {
+  const handleOnlyOpenTasksClick = async () => {
     const onlyOpen = onlyOpenTasks === 2 ? 0 : 2;
     setOnlyOpenTasks(onlyOpen);
     setTodosAux(onlyOpen === 2 ? todos.filter(t => t.status !== 2) : todos.filter(t => t.status === onlyOpen));
@@ -41,7 +47,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <Input callback={callback} />
+      <Input callback={handleUpdateTodos} />
       <div className="flex flex-row items-center justify-center gap-x-2 text-xl ml-5 mt-10">
         <span onClick={handleOnlyOpenTasksClick}>
           {
@@ -56,7 +62,7 @@ export default function Home() {
       </div>
       <Card isLoading={isLoading}>
         <div>
-          {todosAux.length !== 0 ? todosAux.map(t => <TodoItem key={t.id} id={t.id} status={t.status} title={t.title} callback={callback} />) :
+          {todosAux.length !== 0 ? todosAux.map(t => <TodoItem key={t.id} id={t.id} status={t.status} title={t.title} callback={handleUpdateTodos} />) :
             <h2 className="flex items-center justify-center text-black text-5xl h-40">Nothing to show 😔</h2>}
         </div>
       </Card>
